2575684c9150c1106edfe841de7bb496c124be3c
Anton-Mu <1853297280@qq.com>
2023-03-16 19:54:01 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(":5000") // 监听并在 0.0.0.0:5000 上启动服务
+}
